b31309805daa3760430f9a737039135b8bd6bf39
Peter J. Gonzalez <ptr.jgz@gmail.com>
2021-05-28 23:03:52 -0400

    Create the homepage template

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
+const homeRoutes = require('./home-routes.js');
 
 // this finds and uses index.js in api folder
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
